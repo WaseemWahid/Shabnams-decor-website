956638d4223b050585e7fca3b810c618d774e055
Waseem <wahid.waseem@gmail.com>
2022-08-25 23:08:21 -0700

    added title and lorem to about section

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -4,7 +4,12 @@ import { AppWrap, MotionWrap } from '../../wrapper'
 const About = () => {
     return (
         <div className='about__app'>
-            <h1>About the Owner....</h1>
+            {/* Need to get information from client on what she wants to say here */}
+            {/* also Will need a picture of client she is comfortable to upload  */}
+            <h1 className='head-text'>About the Owner....</h1>
+            <p className='p-text'>Shabnams decor was started in 2021 to give the people in the bay area a chance to enjoy a wonderful wedding
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae repellat possimus soluta nostrum doloribus distinctio velit minus nam ipsa eos magni voluptatem a, et at expedita ex culpa. Voluptatum, aliquid.
+            </p>
         </div>
     )
 }
