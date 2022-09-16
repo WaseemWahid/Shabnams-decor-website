81a62b16008cc16f0035af509dbf0e9aa82aee0c
Waseem <wahid.waseem@gmail.com>
2022-09-15 21:18:42 -0700

    Footer section layout completed

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { MotionWrap } from '../../wrapper';
 import { ContactMe } from '../../components';
+import { FaFacebookF } from 'react-icons/fa'
+import { BsInstagram, BsFillHeartFill } from 'react-icons/bs'
 import './Footer.css'
 
 const Footer = () => {
@@ -9,13 +11,27 @@ const Footer = () => {
             <div className='app__footer'>
                 <div className='app__footer-title'>
                     <h1 className='head-text'>Contact Us</h1>
-                    <h3 className='sub-text'>We'd love to Help!</h3>
+                    <h3 className='sub-text'>We'd <BsFillHeartFill/> to Help!</h3>
                 </div>
                 <div className='app__footer-contact'>
                     <ContactMe />
                 </div>
+                <div className='app__footer-social'>
+                    <p className='p-text'>Follow us on Facebook and Instagram!</p>
+                    <div className='app__footer-links'>
+                        <div>
+                            <a><FaFacebookF /> </a>
+                        </div>
+                        <div>
+                            <a><BsInstagram /></a>
+                        </div>
+                        
+                    </div>
+                </div>
                 <div className='app__footer-copyright'>
-                    <p className='p-text'>Copyright @2022</p>
+                    <p className='p-text'>
+                        @2022 SHABNAMS DECOR AND EVENTS ALL RIGHTS RESERVED
+                    </p>
                 </div>
             </div>
         </section>
