462e6627fc07ea6ea8984e36cda3fdc5dca9fd94
Waseem <wahid.waseem@gmail.com>
2022-09-21 20:05:14 -0700

    about section image change and media screen change

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,7 +1,7 @@
 import bgVideo from '../assets/bgVideo.mp4';
 import testLogo from '../assets/temp-logo.png';
 import phone from '../assets/phone.png';
-import aboutImg from '../assets/about-img.png'
+import aboutImg from '../assets/shabnam-decor-about-image.jpg'
 import email from '../assets/contact-mail.png'
 
 export default {
